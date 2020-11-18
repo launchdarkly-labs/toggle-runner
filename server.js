a7efbd7ffa94a552bfc65dc08b68899ec639fc2f
Rich Manalang <rmanalang@launchdarkly.com>
2020-11-18 14:33:50 -0800

    Lesson 1: Space Mode (server-side flag)

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 require('dotenv').config()
+const LaunchDarkly = require('launchdarkly-node-server-sdk')
+const ldclient = LaunchDarkly.init(process.env.LD_SDK_KEY)
+
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT
 
+const LD_USER = {
+  key: `${require('./package.json').name}-${require('os').hostname}`,
+}
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  res.render('game')
+app.get('/', async (req, res) => {
+  try {
+    const isSpaceModeEnabled = await ldclient.variation('space-mode', LD_USER, false)
+    res.render('game', { isSpaceModeEnabled })
+  } catch (err) {
+    console.error(err)
+    res.status(500).send('Error occurred while retrieving flags')
+  }
 })
 
 app.listen(PORT, () => {
